4fefc3332af397afbaa03f49e8a3c60f1a341174
adjay27 <adityawhyjayaa@gmail.com>
2024-03-14 23:06:13 +0700

    feat: complete hero section

--- src/pages/HeroSection.tsx
+++ src/pages/HeroSection.tsx
@@ -1,23 +1,45 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { GlobeIcon } from "@radix-ui/react-icons";
 
 export default function HeroSection() {
-    return (
-        <main className="flex min-h-screen flex-row items-center justify-center">
-            <div className="flex-1 flex-col items-center justify-center ">
-                <div className="text-center">
-                    <h1 className="text-5xl font-bold">Hero TXT</h1>
-                </div>
-                <div className="text-center">
-                    <p>hero desc</p>
-                </div>
-                <div className="text-center">
-                    <Button>Get Started</Button>
-                </div>
+  return (
+    <main className="flex min-h-screen flex-row items-center justify-center">
+      <div className="flex-1 flex-col items-center justify-center">
+        <div className="items-center justify-items-center justify-end max-w-xl px-10 mx-auto">
+          <div className="flex">
+            <h1 className="text-7xl font-medium text-wrap">
+              Revoluzioned Customer Relationship
+            </h1>
+          </div>
+          <div className="mt-2">
+            <h6 className="text-gray-500 ">
+              Streamline Operations, Boost Efficiency, and Drive Growth
+            </h6>
+          </div>
+          <div className="flex flex-col gap-y-5">
+            <div className="mt-4 flex gap-4">
+              <Button>Get Started</Button>
+              <Button variant="outline" className="border-gray-700">
+                Learn More
+              </Button>
             </div>
-            <div className="flex-1 flex  justify-center">
-                <Image src="/hero-dashboard.jpg" alt="hero-img" width={894} height={625} priority />
+            <div className="flex gap-1 align-middle">
+              <p className="text-sm text-gray-500">Used and helping over more <strong className="text-black">200+ Companies </strong></p>
+              <GlobeIcon className="h-4 w-4 my-auto" />
             </div>
-        </main>
-    )
+          </div>
+        </div>
+      </div>
+      <div className="flex-1 flex  justify-center">
+        <Image
+          src="/hero-dashboard.jpg"
+          alt="hero-img"
+          width={894}
+          height={625}
+          priority
+        />
+      </div>
+    </main>
+  );
 }
---
+++ src/pages/SocialProof.tsx
@@ -0,0 +1,15 @@
+export default function SocialProof() {
+  return (
+    <main className="flex min-h-[400px] flex-row items-center justify-center">
+      <div className="flex-1 flex-col items-center  justify-center gap-y-5">
+        <div className="items-center justify-items-center justify-end max-w-xl px-10 mx-auto">
+          <h2 className="text-5xl ">Trusted by Many Established Companies</h2>
+          <p>
+            20+ Businesses and Companies uses Converge for theirs CRM Platform
+          </p>
+        </div>
+      </div>
+      <div className="flex-1 flex  justify-center">Slack icon</div>
+    </main>
+  );
+}
